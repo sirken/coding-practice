97b7bf6952cbd638600f98663c00d6d654b47e2a
sirken <sirken@users.noreply.github.com>
2020-04-09 15:06:58 -0500

    Find lowest missing positive integer in list

---
+++ daily coding/004-find-missing-integer.py
@@ -0,0 +1,35 @@
+'''
+Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
+You can modify the input array in-place.
+'''
+
+# Count up from 1, check against list
+def find_missing_integer(alist):
+
+    count = 1
+
+    while True:
+        if count not in alist:
+            return count
+        count += 1
+
+# Longer, looping through list with for
+def find_missing_integer(alist):
+
+    count = 1
+
+    while True:
+        chknum = False
+        for i in alist:
+            if i == count:
+                chknum = True
+        if not chknum:
+            return count
+        count += 1
+
+
+print(find_missing_integer([3,4,-1,1]))
+print(find_missing_integer([1,2,0]))
