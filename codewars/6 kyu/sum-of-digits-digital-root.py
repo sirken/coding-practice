888fc801775c8833adfb3e3219b01e83aa252a17
sirken <sirken@users.noreply.github.com>
2020-04-29 19:38:43 -0500

    Added script

---
+++ codewars/6 kyu/sum-of-digits-digital-root.py
@@ -0,0 +1,43 @@
+from Test import Test, Test as test
+
+'''
+In this kata, you must create a digital root function.
+
+A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
+
+Here's how it works:
+
+digital_root(16)
+=> 1 + 6
+=> 7
+
+digital_root(942)
+=> 9 + 4 + 2
+=> 15 ...
+=> 1 + 5
+=> 6
+
+digital_root(132189)
+=> 1 + 3 + 2 + 1 + 8 + 9
+=> 24 ...
+=> 2 + 4
+=> 6
+
+digital_root(493193)
+=> 4 + 9 + 3 + 1 + 9 + 3
+=> 29 ...
+=> 2 + 9
+=> 11 ...
+=> 1 + 1
+=> 2
+'''
+
+def digital_root(n):
+    while len(str(n)) > 1:
+        n = sum([int(x) for x in str(n)])
+    return n
+
+
+
+test.assert_equals( digital_root(16), 7 )
+test.assert_equals( digital_root(456), 6 )
