cf73e5780a32fa66d49ed1182a0ffd626a3bb0fc
sirken <sirken@users.noreply.github.com>
2020-04-30 12:07:21 -0500

    Updated scripts

---
+++ codewars/6 kyu/multiples-of-3-or-5.py
@@ -0,0 +1,23 @@
+from Test import Test, Test as test
+
+'''
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+'''
+
+def solution(number):
+    l = []
+    for x in range(3,number):
+        if x % 3 == 0 or x % 5 == 0:
+            l.append(x)
+    print(l)
+    return sum(l)
+
+
+def solution(number):
+    return sum([x for x in range(3,number) if x % 3 == 0 or x % 5 == 0])
+
+test.assert_equals(solution(10), 23)
