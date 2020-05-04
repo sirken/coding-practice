e2dcb3927f6a519cfc5734916a3b2ce0f650ed5b
sirken <sirken@users.noreply.github.com>
2020-05-03 22:21:48 -0500

    Added script

---
+++ codewars/8 kyu/century-from-year.py
@@ -0,0 +1,30 @@
+from Test import Test, Test as test
+
+'''
+Introduction
+The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+
+Task :
+Given a year, return the century it is in.
+
+Input , Output Examples ::
+centuryFromYear(1705)  returns (18)
+centuryFromYear(1900)  returns (19)
+centuryFromYear(1601)  returns (17)
+centuryFromYear(2000)  returns (20)
+'''
+
+import math
+def century(year):
+    return math.ceil(year/100)
+
+# Top solution
+def century(year):
+    return (year + 99) // 100
+
+test.assert_equals(century(1705), 18, 'Testing for year 1705')
+test.assert_equals(century(1900), 19, 'Testing for year 1900')
+test.assert_equals(century(1601), 17, 'Testing for year 1601')
+test.assert_equals(century(2000), 20, 'Testing for year 2000')
+test.assert_equals(century(356), 4, 'Testing for year 356')
+test.assert_equals(century(89), 1, 'Testing for year 89')
