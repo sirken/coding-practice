9cb09cc0febd1bdb87322683e684cca59230c409
sirken <sirken@users.noreply.github.com>
2020-05-25 17:27:12 -0500

    Added script

---
+++ codewars/6 kyu/stop-spinning-my-words.py
@@ -0,0 +1,23 @@
+from Test import Test, Test as test
+
+'''
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
+'''
+
+def spin_words(sentence):
+    l = []
+    for w in sentence.split(' '):
+        if len(w) > 4:
+            w = ''.join(reversed(w))
+        l.append(w)
+    return ' '.join(l)
+
+# Top answer
+def spin_words(sentence):
+    return " ".join([x[::-1] if len(x) >= 5 else x for x in sentence.split(" ")])
+
+
+test.assert_equals(spin_words("Welcome"), "emocleW")
+test.assert_equals(spin_words("Hey fellow warriors"), "Hey wollef sroirraw")
