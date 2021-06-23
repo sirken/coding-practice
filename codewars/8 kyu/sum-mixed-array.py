80c36847f304e5d998781965fe2393aca4847014
sirken <sirken@users.noreply.github.com>
2021-06-22 21:48:12 -0700

    Added script

--- codewars/8 kyu/sum-mixed-array.py
+++ codewars/8 kyu/sum-mixed-array.py
@@ -3,7 +3,7 @@ from Test import Test, Test as test
 '''
 Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 
-Return your answer as a number.
+Return your answer as a number. 
 '''
 
 def sum_mix(arr):
