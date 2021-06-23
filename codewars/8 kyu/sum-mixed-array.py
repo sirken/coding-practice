1f940ae393ba2f82670ba988222866e93a87bc1c
sirken <sirken@users.noreply.github.com>
2021-06-22 21:43:00 -0700

    Added script

---
+++ codewars/8 kyu/sum-mixed-array.py
@@ -0,0 +1,16 @@
+from Test import Test, Test as test
+
+'''
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+'''
+
+def sum_mix(arr):
+    return sum([int(n) for n in arr])
+
+test.assert_equals(sum_mix([9, 3, '7', '3']), 22)
+test.assert_equals(sum_mix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42)
+test.assert_equals(sum_mix(['3', 6, 6, 0, '5', 8, 5, '6', 2, '0']), 41)
+test.assert_equals(sum_mix(['1', '5', '8', 8, 9, 9, 2, '3']), 45)
+test.assert_equals(sum_mix([8, 0, 0, 8, 5, 7, 2, 3, 7, 8, 6, 7]), 61)
